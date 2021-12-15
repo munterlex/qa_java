2a0acfc0abacd65d44964da719586cb4465dcf12
Aleksandr Muntiev <munteraa@yandex.com>
2021-12-15 10:34:19 +0300

    Correction

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,6 +1,4 @@
 package com.example;
-import com.example.Cat;
-import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -19,7 +17,7 @@ Feline feline;
 @Test
     public void testGetFood() throws Exception{
         Cat cat = new Cat(feline);
-        Mockito.when(cat.getFood()).thenReturn(List.of("Животные","Птицы","Рыба"));
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные","Птицы","Рыба"));
         List <String> actual = cat.getFood();
         List <String> expected = List.of("Животные","Птицы","Рыба");
         assertEquals(expected,actual);}
